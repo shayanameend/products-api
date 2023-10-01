1205e32884551bf49daa420c074ff4d8e63a16a7
shayanameend <shayan.ameen.developer@gmail.com>
2023-10-01 14:30:54 -0700

    Error Handling

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,40 +1,48 @@
-import type { Request, Response } from "express";
+import type { Request, Response, NextFunction } from "express";
 import { prisma } from "../lib/prisma";
 import { checkHashedPassword, getTokenOfUser, hashPassword } from "../lib/auth";
 
-export async function signUp(request: Request, response: Response) {
-  const user = await prisma.user.create({
-    data: {
-      username: request.body.username,
-      password: await hashPassword(request.body.password),
-    },
-  });
+export async function signUp(request: Request, response: Response, next: NextFunction) {
+  try {
+    const user = await prisma.user.create({
+      data: {
+        username: request.body.username,
+        password: await hashPassword(request.body.password),
+      },
+    });
 
-  const token = getTokenOfUser(user);
-  response.status(200).json({ token });
+    const token = getTokenOfUser(user);
+    response.status(200).json({ token });
+  } catch (error) {
+    next(error)
+  }
 }
 
-export async function signIn(request: Request, response: Response) {
-  const user = await prisma.user.findUnique({
-    where: {
-      username: request.body.username,
-    },
-  });
+export async function signIn(request: Request, response: Response, next: NextFunction) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        username: request.body.username,
+      },
+    });
 
-  if (!user) {
-    response.status(401).json({ message: "username or password not valid" });
+    if (!user) {
+      response.status(401).json({ message: "username or password not valid" });
 
-    return;
+      return;
+    }
+
+    const isValid = checkHashedPassword(user?.password!, request.body.password);
+
+    if (!isValid) {
+      response.status(401).json({ message: "username or password not valid" });
+
+      return;
+    }
+
+    const token = getTokenOfUser(user);
+    response.status(200).json({ token });
+  } catch (error) {
+    next(error)
   }
-
-  const isValid = checkHashedPassword(user?.password!, request.body.password);
-
-  if (!isValid) {
-    response.status(401).json({ message: "username or password not valid" });
-
-    return;
-  }
-
-  const token = getTokenOfUser(user);
-  response.status(200).json({ token });
 }
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import type {Request, Response, NextFunction} from "express";
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
@@ -6,6 +7,12 @@ import { enforceAuth } from "./lib/auth";
 
 const app = express();
 
+app.get("/", (request: Request, response: Response, next: NextFunction)=> {
+  setTimeout(()=>{
+    next(new Error("Hello!"));
+  },1)
+})
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
@@ -13,5 +20,8 @@ app.use(morgan("dev"));
 
 app.use("/auth", authRouter);
 app.use("/api", enforceAuth, apiRouter);
+app.use((error: Error, request: Request, response: Response, next: NextFunction)=> {
+  response.json({message: error.message})
+});
 
 export default app;
